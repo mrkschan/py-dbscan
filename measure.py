e621a2ef4c2b7f6bca3795602aee89cded622cca
mrkschan <mr.kschan@gmail.com>
2010-04-14 17:52:18 +0800

    fix majority class bug, turned to use int for counting instead of float

--- measure.py
+++ measure.py
@@ -16,15 +16,17 @@ def cls_err(dataset):
         if freq.has_key(cls_label):
             freq[cls_label] += 1
         else:
-            freq[cls_label] = 1.0
+            freq[cls_label] = 1
 
     # find max prob
-    size     = len(dataset)
-    max_prob = .0
-    max_cls  = None
+    size    = len(dataset)
+    max_f   = 0
+    max_cls = None
     for cls, f in freq.items():
-        if f/size > max_prob:
-            max_prob = f/size
-            max_cls  = cls
+        if f > max_f:
+            max_f   = f
+            max_cls = cls
+        elif f == max_f:
+            max_cls = max_cls + '|' + cls
 
-    return 1 - max_prob, cls
+    return 1 - float(max_f)/size, max_cls
